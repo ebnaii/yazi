c6e497042224d17515cd4b9cecadc644e11aa291
ryanalg <129603297+ryanalg@users.noreply.github.com>
2024-07-07 19:52:32 +0200

    Adding script to create ISO

---
+++ ScriptsISO/build.sh
@@ -0,0 +1,6 @@
+sh /home/build/aports/scripts/mkimage.sh --tag edge \
+	--outdir ~/iso \
+	--arch x86_64 \
+	--repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
+	--extra-repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
+	--profile yazi
